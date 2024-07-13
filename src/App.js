b45c2fb29b7c7d0ae5e6d25c126b2cc1681c2cf8
abdev2002 <abd.mghl.dev@gmail.com>
2024-07-13 11:45:47 +0500

    A bit has changed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import Projects from "./components/Projects";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>My Portfolio</h1>
       </header>
+      <main>
+        {/* <Projects /> */}
+      </main>
+      <footer>
+        <p>&copy; 2024 Code Beyond</p>
+      </footer>
     </div>
   );
 }
