ad3363659b47b128a45782cdef6c76bf880baf63
abdev2002 <abd.mghl.dev@gmail.com>
2024-07-13 11:31:42 +0500

    Updates

---
+++ precache-manifest.4e6aa50d168bdca429bcff40da4166bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b0e122643859382d8d9d90419e3477c",
+    "url": "/CodeBeyond/index.html"
+  },
+  {
+    "revision": "ccec56b403839c3d177c",
+    "url": "/CodeBeyond/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/CodeBeyond/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/CodeBeyond/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "ccec56b403839c3d177c",
+    "url": "/CodeBeyond/static/js/main.d3c40c98.chunk.js"
+  },
+  {
+    "revision": "5bbe865fe4711f7268ee",
+    "url": "/CodeBeyond/static/js/runtime~main.ee13cd31.js"
+  }
+]);
